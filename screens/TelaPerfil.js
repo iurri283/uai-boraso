f46ea2b4df5c71cfdabfa59d45cca675e8d87373
iuri <iuriwerneck10@gmail.com>
2023-07-05 14:30:57 -0300

    TelaPerfil atualizada, falta implementar as funcionalidades. Falta também implementar Logout.

--- screens/TelaPerfil.js
+++ screens/TelaPerfil.js
@@ -1,23 +1,161 @@
+import { useState } from 'react';
 import { View } from 'react-native';
-import { Tabmenu } from '../components/Tabmenu.js';
-import { ImageBackground, StyleSheet, Text } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
 
 export function TelaPerfil() {
+
+  const [mensagem, setMensagem] = useState('');
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [senha, setSenha] = useState('');
+
+
+  function atualizarUsuario(){
+
+  }
+
+  function getUsuario(){
+    
+  }
+
   return (
-    <LinearGradient
-      style={styles.container}
-      colors={['#87CEEB', 'rgba(135, 206, 235, 0)']}>
-      
-    </LinearGradient>
+
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/perfil.png')}
+        style={estilo.perfil}
+      />
+      <View style={estilo.formCadastro}>
+
+        <Text style={estilo.tituloForm}>Cadastro de Carona</Text>
+
+        <TextInput
+          style={estilo.inputForm}
+          placeholderTextColor="white"
+          placeholder="nome"
+          autoCapitalize="none"
+          autoCorrect={true}
+          onChangeText={(event) => setNome(event)}
+        />
+
+        <TextInput
+          style={estilo.inputForm}
+          placeholderTextColor="white"
+          placeholder="email"
+          autoCapitalize="none"
+          autoCorrect={true}
+          onChangeText={(event) => setEmail(event)}
+        />
+
+        <View style={estilo.inputMenor}>
+          <TextInput
+            style={estilo.inputFormTel}
+            placeholderTextColor="white"
+            placeholder="telefone"
+            autoCapitalize="none"
+            autoCorrect={true}
+            onChangeText={(event) => setTelefone(event)}
+          />
+
+          <TextInput
+            style={estilo.inputFormSenha}
+            placeholderTextColor="white"
+            placeholder="Senha"
+            autoCompleteType="current-password"
+            autoCapitalize="none"
+            secureTextEntry={true}
+            onChangeText={(event) => setSenha(event)}
+          />
+        </View>
+        <TouchableOpacity
+          style={estilo.buttonForm}
+          onPress={() => atualizarUsuario()}>
+          <Text style={estilo.textButton}>Cadastrar</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.textAlert}>{mensagem}</Text>
+      </View>
+    </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    justifyContent:"center",
-    width: '100%',
-    height: 711,
+    flex: 1,
+    alignItems: "center",
+    // justifyContent:"center",
+  }
+});
+
+const estilo = StyleSheet.create({
+  perfil: {
+    justifyContent: "center",
+    marginTop: 200,
+    width: 100,
+    height: 100,
+  },
+  formCadastro: {
+    width: "90%",
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    backgroundColor: "white"
+  },
+  tituloForm: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#87CEEB',
+  },
+  inputForm: {
+    backgroundColor: '#87CEEB',
+    marginTop: 13,
+    width: 250,
+    height: 35,
+    borderRadius: 5,
+    padding: 5
+  },
+  inputMenor:{
+    width: 250,
+    alignItems: 'flex-start', // Alterado para alinhar à esquerda
+    display: 'flex',
+    flexDirection: 'column',
+    left: 0
+  },
+  inputFormTel: {
+    backgroundColor: '#87CEEB',
+    marginTop: 13,
+    marginLeft: 0,
+    width: 150,
+    height: 35,
+    borderRadius: 5,
+    padding: 5
+  },
+  inputFormSenha: {
+    backgroundColor: '#87CEEB',
+    marginTop: 13,
+    width: 150,
+    height: 35,
+    borderRadius: 5,
+    padding: 5
+  },
+  teste: {
+    marginTop: 90,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonForm: {
+    backgroundColor: '#87CEEB',
+    width: 100,
+    height: 30,
+    borderRadius: 5,
+    marginTop: 13,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textButton: {
+    color: '#fff',
+    fontWeight: 'bold',
   }
 });
